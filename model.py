541870017de8467f286f47ae2599eb03b165ee09
MiltonCarreno <miltonyaredcr@gmail.com>
2023-01-30 11:44:21 -0500

    Added LayerNorm to inputs of MultiHeadAttention and FeedForward, as well as to the concatenated output from the layers of Block. Also cleaned code.

--- model.py
+++ model.py
@@ -4,14 +4,14 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # --------Hyperparameters---------
-batch_size = 4 # 64 Independent sequences to process in parallel
-block_size = 8 # 256 Max context length for predictions
+batch_size = 48 # 64 Independent sequences to process in parallel
+block_size = 160 # 256 Max context length for predictions
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = torch.device("mps")
 eval_iters = 200
-n_embd = 32 # 384
+n_embd = 192 # 384
 n_head = 4 # 6
 n_layer = 6 # 6
 dropout = 0.2
@@ -19,59 +19,16 @@ dropout = 0.2
 
 torch.manual_seed(1337)
 
-# Get preprocessed dataset
-with open("texts/tiny_borges.txt", "r", encoding="utf-8") as fd:
-    text = fd.read()
-
-# Get list of unique characters in dataset
-chars = sorted(list(set(text)))
-vocab_size = len(chars) # Set vocab_size
-# print(f"Vocab Size: {vocab_size}\n{chars}")
-
-# Encode and decoder for bidirectional character-integer translation
-stoi = {c:i for i, c in enumerate(chars)}
-itos = {i:c for i, c in enumerate(chars)}
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: ''.join([itos[i] for i in l])
-
-# Split data 90/10 for training/validation
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data))
-train_data = data[:n]
-val_data = data[n:]
-
-# Function to get a batch of data from train or val data
-def get_batch(split):
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1]for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-# Function to calculate the loss of model during training
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    m.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = m(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    m.train()
-    return out
-
 class Head(nn.Module):
     """ One head of self-attention """
+
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B,T,C = x.shape # (B,T,n_embd)
@@ -81,6 +38,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B,T,C) @ (B,C,T) --> (B,T,T)
         wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) # (B,T,T)
         wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
         # Weighted aggregation of the values
         v = self.value(x) # (B,T,head_size)
         out = wei @ v # (B,T,T) @ (B,T,head_size) --> (B,T,head_size)
@@ -93,13 +51,15 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         # Each head attends to the entire block,
         # taking input of (B,T,n_embd) and producing out of (B,T,head_size)
         # head_size = n_embd // num_heads
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.proj(out) # Projection applied to output before residual connection
+        out = self.dropout(out)
         return out
 
 class FeedForward(nn.Module):
@@ -108,9 +68,11 @@ class FeedForward(nn.Module):
     def __init__(self, layer_size):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(layer_size, layer_size*4),
+            nn.Linear(layer_size, layer_size*4), # (n_embd,n_embd*4)
             nn.ReLU(),
-            nn.Linear(layer_size*4, layer_size),
+            # Projection applied before residual connection
+            nn.Linear(layer_size*4, layer_size), # (n_embd*4,n_embd)
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -119,27 +81,28 @@ class FeedForward(nn.Module):
 
 class Block(nn.Module):
     """ Transformer block, comprised of communication followed by computation"""
+
     def __init__(self, n_embd, n_heads):
         super().__init__()
         head_size = n_embd // n_heads
         self.sa = MultiHeadAttention(n_heads, head_size) # 4 heads, each (B,T,head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x)) # Residual connection added to output MultiHeadAttention
+        x = x + self.ffwd(self.ln2(x)) # Residual connection added to output FeedForward
         return x
 
 class TinyTransformer(nn.Module):
-    def __init__(self):
+
+    def __init__(self, vocab_size):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head),
-            Block(n_embd, n_head),
-            Block(n_embd, n_head),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # Final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -149,6 +112,7 @@ class TinyTransformer(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,n_embd)
         x = tok_emb + pos_emb # (B,T,n_embd)
         x = self.blocks(x) # (B,T,n_embd)
+        x = self.ln_f(x) # (B,T,n_embd)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
@@ -162,9 +126,17 @@ class TinyTransformer(nn.Module):
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
-        for _ in tqdm(range(max_new_tokens)):
+        idx_cond = 0
+        for i in tqdm(range(max_new_tokens)):
+            if i == 0:
+                idx_cond = idx[:,-1:]
+            elif block_size == idx_cond.size(dim=1):
+                idx_cond = idx[:,-5:]
+            else:
+                idx_cond = torch.cat((idx_cond, idx[:,-1:]), dim=1)
+
             # Crop idx to last block_size tokens
-            idx_cond = idx[:, -block_size:]
+            # idx_cond = idx[:, -block_size:]
             # Get predictions
             logits, loss = self(idx_cond)
             # Get predictions for the last char for all batches
@@ -177,28 +149,3 @@ class TinyTransformer(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
     
-model = TinyTransformer()
-m = model.to(device) 
-
-# Optimizer
-optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
-
-# Training loop
-for iter in tqdm(range(max_iters)):
-    # Evaluate loss of model every 'eval_interval'
-    if iter % eval_interval == 0 or iter == (max_iters - 1):
-        losses = estimate_loss()
-        print("\n-----")
-        print(f"Step {iter}\nTrain Loss: {losses['train']:.4f}; Val Loss: {losses['val']:.4f}")
-        
-    xb, yb = get_batch('train') # Get batch of data
-    logits, loss = m(xb, yb) # Get prediction
-    optimizer.zero_grad(set_to_none=True) # Zero previous gradient values
-    loss.backward() # Calculate new gradients for all parameters
-    optimizer.step() # Update parameters according to gradient
-
-# Generate from model
-idx = torch.zeros((1, 1), dtype=torch.long, device=device)
-print("\n\n********Predictions********")
-print(decode(m.generate(idx, max_new_tokens=500)[0].tolist()))
-print("***************************\n")
